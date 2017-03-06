8c5efd641d3734a9db5ebbdcbc63b8230b63196d
Aditya Jain <aditjain13@gmail.com>
2017-03-05 20:48:41 -0800

    Create Database
    + Built migrations for the DB according to ER diagram
    + Run `php artisan migrate` to update

---
+++ database/migrations/2017_03_06_043321_create_listings_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('userId')->unsigned();
+            $table->foreign('userId')->references('id')->on('users');
+
+            $table->integer('catId')->unsigned();
+            $table->foreign('catId')->references('id')->on('categorys');
+
+            $table->bigInteger('isbn');
+            $table->string('name');
+            $table->integer('edition');
+            $table->decimal('price',8,2);
+            $table->string('condition');
+            $table->text('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listings');
+    }
+}
---
+++ database/migrations/2017_03_06_044024_create_sales_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('buyerId')->unsigned();
+            $table->foreign('buyerId')->references('id')->on('users');
+
+            $table->integer('listingId')->unsigned();
+            $table->foreign('listingId')->references('id')->on('listings');
+
+            $table->dateTime('meetingTime');
+            $table->string('location');
+            $table->string('note');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
