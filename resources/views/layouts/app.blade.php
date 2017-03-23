25075dbc61a1615f195994521f3525220dec511e
Aditya Jain <aditjain13@gmail.com>
2017-03-22 22:03:39 -0700

    Link pages together
    + Linking some pages together, using layouts.app as master UI file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
     <!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([
@@ -45,6 +41,7 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
+                            <li><a href = "{{ URL::to('/') }}">All Listings</a></li>
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
--- resources/views/listing/index.blade.php
+++ resources/views/listing/index.blade.php
@@ -1,50 +1,56 @@
-
-@extends('layout')
-
+@extends('layouts.app')
 @section('content')
+<div class = "container">
+    <div class = "row">
+        <div class = "col-md-12">
+            <b>uBooks: books for u</b><br>
+            <form action="{{action('ListingController@showCategoryOnly')}}">
+                <select name="categories">
+                    <option value="0">All Subjects</option>
+                    <option value="1">Biology</option>
+                    <option value="2">Business</option>
+                    <option value="3">Computer Science</option>
+                    <option value="4">Education</option>
+                    <option value="5">English</option>
+                    <option value="6">Engineering</option>
+                    <option value="7">Human Kinetics</option>
+                    <option value="8">Mathematics</option>
+                    <option value="9">Physics</option>
+                </select>
+                <button type="submit">Submit</button>
+            </form>
 
-    <b>uBooks: books for u</b><br>
-    <form action="{{action('ListingController@showCategoryOnly')}}">
-        <select name="categories">
-            <option value="0">All Subjects</option>
-            <option value="1">Biology</option>
-            <option value="2">Business</option>
-            <option value="3">Computer Science</option>
-            <option value="4">Education</option>
-            <option value="5">English</option>
-            <option value="6">Engineering</option>
-            <option value="7">Human Kinetics</option>
-            <option value="8">Mathematics</option>
-            <option value="9">Physics</option>
-    </select>
-        <button type="submit">Submit</button>
-    </form>
-    <br>
-    <a href = "{{URL::to('create')}}"><button>Create New Listing</button></a>
-    <br>
-
-    <table>
-        <tr>
-        <th>Title</th>
-        <th>Edition</th>
-        <th>Condition</th>
-        <th>Price</th>
-        </tr>
-   @foreach ($booklistings as $listing)
-        <tr>
-            <td><a href="listing/{{ $listing->id }}" class="rowlink">
-            {{ $listing->name }}
-            </a></td>
-            <td>
-            {{ $listing->edition }}
-            </td>
-            <td>
-            {{ $listing->quality }}
-            </td>
-            <td>
-            {{ $listing->price }}
-            </td>
-        </tr>
-    @endforeach
-    </table>
+            @if(Auth::User())
+                <br>
+                <a href = "{{URL::to('create')}}"><button>Create New Listing</button></a>
+                <br>
+            @endif
+            <br>
+            <table class = "col-md-12 table-striped">
+                <tr>
+                    <th>Title</th>
+                    <th>Edition</th>
+                    <th>Condition</th>
+                    <th>Price</th>
+                </tr>
+                @foreach ($booklistings as $listing)
+                    <tr>
+                        <td><a href="listing/{{ $listing->id }}" class="rowlink">
+                                {{ $listing->name }}
+                            </a></td>
+                        <td>
+                            {{ $listing->edition }}
+                        </td>
+                        <td>
+                            {{ $listing->condition }}
+                        </td>
+                        <td>
+                            {{ $listing->price }}
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
+</div>
 @stop
