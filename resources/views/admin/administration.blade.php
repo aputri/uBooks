d37468896edbdd31742d90ef8c3acf40af637220
Nadine Haddad <nadinehaddad96@gmail.com>
2017-03-23 00:15:07 -0700

    change view

--- resources/views/admin/administration.blade.php
+++ resources/views/admin/administration.blade.php
@@ -1,62 +1,55 @@
-@extends('layout')
+@extends('layouts.app')
 
 @section('content')
-	<table>
-		<tr>
-			<th>
-				ID
-			</th>
-			<th>
-				User
-			</th>
-			<th>
-				Email
-			</th>
-			<th>
-				Admin
-            </th>
-            <th>
-                Status
-			<th>
-				Delete
-			</th>
-			<th>
-				Ban
-			</th>
-		</tr>
-		 @foreach ($users as $user)
-        <tr>
-        	<td>
-        	{{ $user->id }}
-            </td>
-            <td>
-            {{ $user->name }}
-            </a></td>
-            <td>
-            {{ $user->email }}
-            </td>
-            <td>
-            @if($user->admin)
-                Yes
-            @endif
-            </td>
-            <td>
-            @if($user->banned)
-                Banned
-            @endif
-            </td>
-            <td>
-            	<a href="/administration/{{ $user->id }}">
-            		Yes
-            	</a>
-            </td>
-            <td>
-                <a href="/administration/ban/{{ $user->id }}">
-                    Yes
-                </a>
-            </td>
-        </tr>
-    	@endforeach
-    </table>
-
+    <div class = "container">
+        <div class = "row">
+            <div class = "col-md-12 panel">
+                <table class = "table-striped col-md-12">
+                    <tr>
+                        <th> ID </th>
+                        <th> User </th>
+                        <th> Email </th>
+                        <th>  Admin  </th>
+                        <th> Status </th>
+                        <th> Delete </th>
+                        <th> Ban </th>
+                        <th> Change Password </th>
+                    </tr>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td> {{ $user->id }}  </td>
+                            <td> {{ $user->name }}  </td>
+                            <td> {{ $user->email }}  </td>
+                            <td>
+                                @if($user->admin)
+                                    Yes
+                                @endif
+                            </td>
+                            <td>
+                                @if($user->banned)
+                                    Banned
+                                @endif
+                            </td>
+                            <td>
+                                <a href="{{URL::to('/administration/'. $user->id ) }}">
+                                    Yes
+                                </a>
+                            </td>
+                            <td>
+                                <a href="{{URL::to('/administration/ban/' . $user->id ) }}">
+                                    Yes
+                                </a>
+                            </td>
+                            <td>
+                                <form method = "post" action = "{{URL::to('/administration/changePass/' . $user->id) }}">
+                                    <input type = "password" name = "password" />
+                                    <input type = "submit" value = "Change Password">
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
 @stop
